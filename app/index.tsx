130a9266aac393a3cf86be77ef66e2b294ae2d3f
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-30 22:32:04 -0300

    basic router scraps

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
-import {Text, View} from 'react-native'
+import {Pressable, Text, View} from 'react-native'
+import {Link, router} from 'expo-router'
 
 function HomePage(){
-    return <View><Text>Home Page</Text></View>
+    return (
+        <View>
+            <Text>Home Page</Text>
+            <Link href="/users/1">Go to user 1</Link>
+            <Pressable onPress={() => router.push("/users/2")}>
+                <Text>Go to user 2</Text>
+            </Pressable>
+        </View>
+    )
 }
 
 export default HomePage;
---
+++ app/users/[id].tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function UserPage(){
+    return (
+        <View>
+            <Text>User Page</Text>
+        </View>
+    )
+}
+
+export default UserPage;
