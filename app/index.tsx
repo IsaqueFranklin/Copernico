dfde40f27bd1e940c0dcc8c4130d0bd61d617d22
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-05-01 22:12:05 -0300

    last commit fix

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,11 @@ function HomePage(){
             <Pressable onPress={() => router.push("/users/2")}>
                 <Text>Go to user 2</Text>
             </Pressable>
+
+            <Link href="/website/1">Go to website 1</Link>
+            <Pressable onPress={() => router.push("/website/2")}>
+                <Text>Go to website 2</Text>
+            </Pressable>
         </View>
     )
 }
