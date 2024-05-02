e53be0d8dfe2c4fe80a533f90200fe5f63218ce2
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-05-02 12:39:20 -0300

    starting to code tabs navigation

--- app/website/[web].tsx
+++ app/website/[web].tsx
@@ -1,9 +1,14 @@
-import {Text, View} from 'react-native';
+import {Text, View, Pressable} from 'react-native';
+import {router, Link} from 'expo-router';
 
 function WebsitePage(){
     return (
         <View>
             <Text>User Page</Text>
+            <Link href="/">Back to home page.</Link>
+            <Pressable onPress={() => router.push("/")}>
+                <Text>Back to home page.</Text>
+            </Pressable>
         </View>
     )
 }
